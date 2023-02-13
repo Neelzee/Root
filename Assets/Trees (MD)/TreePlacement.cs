42557c90e9bf0d479b76dfd305ab7e0ed49b903c
Mikal Drivenes <mikal-dr@hotmail.com>
2023-02-13 18:46:31 +0100

    Added highlight for placement, and made placement 'snappier'

--- Assets/Trees (MD)/TreePlacement.cs
+++ Assets/Trees (MD)/TreePlacement.cs
@@ -11,8 +11,13 @@ public class TreePlacement : MonoBehaviour
     [SerializeField] 
     private GameObject treeType;
     private GameObject _currentTree;
+    private Vector3Int _lastCellPos;
     [SerializeField]
     private Tilemap tilemap;
+
+    [SerializeField] private Tilemap highlightMap;
+
+    [SerializeField] private Tile _highlight;
 
     private void Start()
     {
@@ -24,7 +29,7 @@ public class TreePlacement : MonoBehaviour
         _currentTree = newTree;
     }
 
-    private GameObject CheckForTree(Vector3Int cellPos)
+    private GameObject CheckOccupied(Vector3Int cellPos)
     {
         for (int i = 0; i < transform.childCount; i++)
         {
@@ -42,6 +47,8 @@ public class TreePlacement : MonoBehaviour
         //Finds the mouse position and translate it
         var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         var holdPos = new Vector3(mousePos.x, mousePos.y, 0);
+        var cellPos = tilemap.WorldToCell(holdPos);
+        bool isOccupied = CheckOccupied(cellPos);
         
         // For testing, this will be handled with an UI later.
         if (Input.GetKeyDown(KeyCode.A))
@@ -51,28 +58,49 @@ public class TreePlacement : MonoBehaviour
                 Destroy(_currentTree);
             }
             HoldTree(treeType);
+            _currentTree.transform.position = tilemap.GetCellCenterWorld(cellPos);;
+            highlightMap.SetTile(cellPos, _highlight);
+        }
+
+        if (isOccupied)
+        {
+            _highlight.color = Color.red;
+        }
+        else
+        {
+            _highlight.color = Color.white;
         }
         
         // Places the tree if Mouse0 is pressed, change this to accomodate for keybinding options.
         if (_currentTree != null)
         {
-            _currentTree.transform.position = holdPos;
+            _currentTree.transform.position = tilemap.GetCellCenterWorld(cellPos);;
             if (Input.GetKeyDown(KeyCode.Mouse0))
             {
-                var cellPos = tilemap.WorldToCell(holdPos);
-                if (CheckForTree(cellPos) == null)
+                if (CheckOccupied(cellPos) == null)
                 {
                     _currentTree.transform.position = tilemap.GetCellCenterWorld(cellPos);
                     tilemap.SetTile(cellPos, _currentTree.GetComponent<TreeInfo>().getTile());
                     _currentTree.name = "Tree: " + cellPos;
                     _currentTree.GetComponent<SpriteRenderer>().enabled = false;
                     _currentTree = null;   
+                    highlightMap.SetTile(_lastCellPos, null);
+                }
+            }
+            else
+            {
+                if (cellPos != _lastCellPos)
+                {
+                    highlightMap.SetTile(_lastCellPos, null);
+                    highlightMap.SetTile(cellPos, _highlight);
+                    _lastCellPos = cellPos;
                 }
             }
 
             if (Input.GetKeyDown(KeyCode.Mouse1))
             {
                 Destroy(_currentTree);
+                highlightMap.SetTile(_lastCellPos, null);
             }
         }
         else
@@ -80,8 +108,7 @@ public class TreePlacement : MonoBehaviour
             // Checks if the player clicks on a tile with a tree
             if (Input.GetKeyDown(KeyCode.Mouse0))
             {
-                var cellPos = tilemap.WorldToCell(holdPos);
-                GameObject selectedTree = CheckForTree(cellPos);
+                GameObject selectedTree = CheckOccupied(cellPos);
             }
         }
     }
