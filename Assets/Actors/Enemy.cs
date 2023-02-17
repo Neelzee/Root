af8ab8c291643184f71640f99ac070a1d713bb85
Nils Michael <nfi005@uib.no>
2023-02-17 06:05:45 +0100

    Implemented CA, ATM has no effect, but when implementing future enemies, they can read this value and change their state accordingly

--- Assets/Actors/Enemy.cs
+++ Assets/Actors/Enemy.cs
@@ -1,7 +1,16 @@
 using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
 
 public class Enemy : Actor
 {
+    [Header("Climate Awareness")]
+    [Tooltip("Likelihood of this unit's death increasing Climate Awareness")]
+    [SerializeField] protected float caIncrementChance;
+    [Tooltip("How much Climate Awareness is increased if this unit is killed")]
+    [SerializeField] protected float caValue;
+    
     public override void TakeDamage(int damage)
     {
         CurrentHealth -= damage;
@@ -13,6 +22,10 @@ public class Enemy : Actor
 
     public override void Die()
     {
+        if (Random.Range(0, 100) / 100f < caIncrementChance)
+        {
+            ClimateAwareness.Add(caValue);
+        }
         Destroy(gameObject);
     }
 }
---
+++ Assets/Climate Awareness (NMF)/ClimateAwareness.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ClimateAwareness
+{
+    /// <summary>
+    /// Ranges from 0 to 1
+    /// </summary>
+    private static float _caValue;
+
+    /// <summary>
+    /// The Climate Awareness Value, ranges from 0 to 1
+    /// </summary>
+    public static float ClimateAwarenessValue => _caValue;
+
+    /// <summary>
+    /// Adds the given value, to the climate awareness value
+    /// </summary>
+    /// <param name="val"></param>
+    public static void Add(float val)
+    {
+        _caValue = Mathf.Clamp01(_caValue + val);
+    }
+
+    /// <summary>
+    /// <para>
+    /// Scales the value based on the given percentage.
+    /// </para>
+    /// <para>
+    /// ScaleValue(.5f) -> CA val is increased with 50%
+    /// </para>
+    /// <para>
+    /// ScaleValue(-0.5f) -> CA val is decreased with 50%
+    /// </para>
+    /// </summary>
+    /// <param name="percentage"></param>
+    public static void ScaleValue(float percentage)
+    {
+        _caValue = Mathf.Clamp01(percentage * _caValue);
+    }
+}
