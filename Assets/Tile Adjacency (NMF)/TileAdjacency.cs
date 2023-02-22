c5062a18e93f7adaf5e9111bbd4b4358672ea651
neelzee <nilsien2001@gmail.com>
2023-02-22 15:45:03 +0100

    Added comments and a function to get the distance to the closest lake tile for any point on the tilemap

--- Assets/Tile Adjacency (NMF)/TileAdjacency.cs
+++ Assets/Tile Adjacency (NMF)/TileAdjacency.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 /// <summary>
 /// Returns the tile adjacency for a specific position
@@ -11,6 +12,13 @@ using UnityEngine;
 /// </summary>
 public static class TileAdjacency
 {
+    /// <summary>
+    /// Returns an array of the surrounding biomes values, first element is moistness, second is temperature.
+    /// </summary>
+    /// <param name="pos"></param>
+    /// <param name="radius"></param>
+    /// <returns></returns>
+    /// <exception cref="IndexOutOfRangeException">If the given point is out of range</exception>
     public static (int, int)[,] GetAdjacencyValues(Vector3Int pos, int radius = 1)
     {
         if (pos.x < 0 || pos.y < 0 || pos.x >= World.MoistnessMap.GetLength(0) ||
@@ -33,5 +41,52 @@ public static class TileAdjacency
         }
 
         return arr;
+    }
+
+    /// <summary>
+    /// Calculates the euclidean distance between a given point and the closet lake tile
+    /// TODO: Improve this function with a better algorithm
+    /// </summary>
+    /// <param name="pos"></param>
+    /// <param name="maxDistToCheck">How far away too check for lake</param>
+    /// <returns></returns>
+    /// <exception cref="MissingComponentException">If the tilemap component cannot be found</exception>
+    public static float DistanceToClosestLake(Vector3Int pos, float maxDistToCheck = 100f)
+    {
+        Tilemap tm = null;
+        var obj = GameObject.FindGameObjectWithTag("Grid");
+        for (int i = 0; i < obj.transform.childCount; i++)
+        {
+            if (obj.transform.GetChild(i).name == "Lake")
+            {
+                tm = obj.transform.GetChild(i).GetComponent<Tilemap>();
+            }
+        }
+
+        if (tm == null)
+        {
+            throw new MissingComponentException("Cant find Lake Tilemap");
+        }
+
+        float minDist = float.MaxValue;
+        for (float x = pos.x - maxDistToCheck; x < pos.x + maxDistToCheck + 1; x++)
+        {
+            for (float y = pos.y - maxDistToCheck; y < pos.y + maxDistToCheck + 1; y++)
+            {
+                var p = new Vector3Int((int)x, (int)y, 0);
+                if (tm.GetTile<Tile>(p) != null)
+                {
+                    float dist = Vector3Int.Distance(pos, p);
+                    if (dist >= minDist)
+                    {
+                        continue;
+                    }
+
+                    minDist = dist;
+                }
+            }
+        }
+
+        return minDist;
     }
 }
